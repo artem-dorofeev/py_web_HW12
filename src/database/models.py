d50aa4eca6be4eba6a38b775b4581bb990a21adb
Artem Dorofeev <130536391+artem-dorofeev@users.noreply.github.com>
2023-11-19 18:41:07 +0200

    copy hw11

---
+++ src/database/models.py
@@ -0,0 +1,18 @@
+from sqlalchemy import Boolean, Column, ForeignKey, Integer, String, DateTime, func, event, Date
+from sqlalchemy.orm import relationship, declarative_base
+
+Base = declarative_base()
+
+
+class Contact(Base):
+    __tablename__ = "contacts"
+    id = Column(Integer, primary_key=True)
+    firstname = Column(String, index=True)
+    lastname = Column(String)
+    email = Column(String, unique=True, index=True)
+    phone = Column(String, default="None", nullable=False)
+    birthday = Column(Date, default=None, nullable=True)
+    additional = Column(String, default="None", nullable=False)
+    created_at = Column(DateTime, default=func.now())  
+    updated_at = Column(DateTime, default=func.now(), onupdate=func.now())
+
