e4eb9f828928684344f1b0c6200c59354dc7257c
Artem Dorofeev <130536391+artem-dorofeev@users.noreply.github.com>
2023-11-19 22:09:16 +0200

    add auth, upd modes, schemas

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import time
-from fastapi import FastAPI, Depends, HTTPException, Request
+from fastapi import FastAPI, Depends, HTTPException, Request, status
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
@@ -8,7 +8,7 @@ from sqlalchemy import text
 
 
 from src.database.db import get_db
-from src.routes import contacts
+from src.routes import contacts, auth
 
 app = FastAPI()
 
@@ -35,15 +35,17 @@ async def root(request: Request):
 @app.get("/api/healthchecker")
 def healthchecker(db: Session = Depends(get_db)):
     try:
-        # Make request
         result = db.execute(text("SELECT 1")).fetchone()
         if result is None:
-            raise HTTPException(status_code=500, detail="Database is not configured correctly")
+            raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                                detail="Database is not configured correctly")
         return {"message": "Welcome to FastAPI!"}
     except Exception as e:
         print(e)
-        raise HTTPException(status_code=500, detail="Error connecting to the database")
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                            detail="Error connecting to the database")
 
 
 app.include_router(contacts.router, prefix='/api')
+app.include_router(auth.router, prefix='/api')
 
--- src/database/models.py
+++ src/database/models.py
@@ -1,7 +1,16 @@
-from sqlalchemy import Boolean, Column, ForeignKey, Integer, String, DateTime, func, event, Date
+import enum
+
+from sqlalchemy import Boolean, Column, ForeignKey, Integer, String, DateTime, func, event, Date, Enum
 from sqlalchemy.orm import relationship, declarative_base
 
+
 Base = declarative_base()
+
+class Role(enum.Enum):
+    admin: str = 'admin'
+    moderator: str = 'moderator'
+    user: str = 'user'
+
 
 
 class Contact(Base):
@@ -15,4 +24,19 @@ class Contact(Base):
     additional = Column(String, default="None", nullable=False)
     created_at = Column(DateTime, default=func.now())  
     updated_at = Column(DateTime, default=func.now(), onupdate=func.now())
+    user_id = Column(
+        "user_id", ForeignKey("users.id", ondelete="CASCADE"), default=None
+    )
+    user = relationship("User", backref="notes")
 
+
+class User(Base):
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True)
+    username = Column(String(50))
+    email = Column(String(250), nullable=False, unique=True)
+    password = Column(String(255), nullable=False)
+    created_at = Column("crated_at", DateTime, default=func.now())
+    avatar = Column(String(255), nullable=True)
+    refresh_token = Column(String(255), nullable=True)
+    roles = Column('role', Enum(Role), default=Role.user)
